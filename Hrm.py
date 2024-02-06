4236c380c2b1b80f297535c2094e60772e37ace4
Admin <azeesalfan2169@gmail.com>
2024-02-06 02:47:05 -0800

    Initial commit

---
+++ Hrm.py
@@ -0,0 +1,25 @@
+class Employee:
+    def __init__(self, Emp_Id, Emp_Name, Emp_dep, Emp_contact, Emp_app, Emp_status):
+
+        self.Emp_id = Emp_Id
+        self.Emp_Name = Emp_Name
+        self.Emp_dep = Emp_dep
+        self.Emp_contact = Emp_contact
+        self.Emp_app = Emp_app
+        self.Emp_status = Emp_status
+
+def add_employee(self , Emp_id, Emp_Name, Emp_dep, Emp_contact, Emp_app, Emp_status)
+
+     pass
+
+def view_emp_info(self):
+    pass
+
+def update_emp_info(self):
+    pass
+
+def leave_request(self):
+    pass
+
+def view_company_policy(self)
+    pass
